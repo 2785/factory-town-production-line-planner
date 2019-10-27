c853bcda768c51a1231343d2a3f949841372f8df
Zhao Shiqi <zhao.shiqi.art@gmail.com>
2019-10-27 13:25:47 -0400

    Add resolver for getting production line

--- functions/src/apollo/resolvers/productionLinePlannerResolver.ts
+++ functions/src/apollo/resolvers/productionLinePlannerResolver.ts
@@ -1,18 +1,29 @@
 import { Resolvers, ProductionLineResponse } from "../generated/types";
+import { ApolloServerContext } from "../server";
+import { ProductionLinePlannerService } from "../../services/productionLinePlanner/productionLinePlannerService";
 
 export const productionLinePlannerResolver: Resolvers = {
     Query: {
-        getProductionLine: (obj, args, context, info) => {
-            const res: ProductionLineResponse = { productionSteps: [] };
+        getProductionLine: (obj, args, context: ApolloServerContext, info) => {
+            const serviceProvider = new ProductionLinePlannerService(
+                context.recipeDataSource
+            );
+
+            const res: ProductionLineResponse = {
+                productionSteps: serviceProvider.getProductionLine(
+                    args.requirement.product,
+                    args.requirement.count
+                )
+            };
             return Promise.resolve(res);
         },
         hello: () => Promise.resolve("Hello!")
     },
     Mutation: {
-        addProduct: (obj, args, context, info): boolean => {
+        addProduct: (obj, args, context: ApolloServerContext, info) => {
             return false;
         },
-        addSpecialFacility: (obj, args, context, info) => {
+        addSpecialFacility: (obj, args, context: ApolloServerContext, info) => {
             return Promise.resolve(false);
         }
     }
