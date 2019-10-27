63694960303330a9fd0ce73e5c5baad22e9267bc
Zhao Shiqi <zhao.shiqi.art@gmail.com>
2019-10-27 12:29:27 -0400

    Fix config json path

--- functions/src/utilities/firebaseUtil.ts
+++ functions/src/utilities/firebaseUtil.ts
@@ -2,7 +2,7 @@ import * as admin from "firebase-admin";
 import * as functions from "firebase-functions";
 import * as fs from "fs-extra";
 
-const configFilePath = "config.json";
+const configFilePath = "fbCred.json";
 const databaseURL = "https://factory-town-prod-line-planner.firebaseio.com";
 
 export async function initializeIfNotAlreadyInitialized(): Promise<void> {
@@ -10,7 +10,7 @@ export async function initializeIfNotAlreadyInitialized(): Promise<void> {
         if (process.env.NODE_ENV == "production") {
             admin.initializeApp(functions.config().firebase);
         } else {
-            const config = await fs.readJson(configFilePath);
+            const config = await fs.readJson(`src/utilities/${configFilePath}`);
             admin.initializeApp({
                 credential: admin.credential.cert(config),
                 databaseURL: databaseURL
