1f4f3def061a0ab57f33528c84d7b100e5614bee
Zhao Shiqi <zhao.shiqi.art@gmail.com>
2019-11-01 14:48:39 -0400

    Start on a private method in dataprocessor

--- functions/src/services/dataProcessor/dataProcessor.ts
+++ functions/src/services/dataProcessor/dataProcessor.ts
@@ -1,5 +1,17 @@
-import { Database } from "../../dataSources/getDataSources";
+import { IDatabaseEngine } from "../../dataSources/backEndDataAccess/iDataBaseEngine";
+import * as path from "path";
+import * as os from "os";
+import { initializeIfNotAlreadyInitialized } from "../../utilities/firebaseUtil";
+import * as admin from "firebase-admin";
 
 export class DataProcessor {
-    constructor(private dbEngine: Database) {}
+    constructor(private dbEngine: IDatabaseEngine) {}
+
+    private async processAndUploadCsv<T>(dbPath: string, csvFilePath: string) {
+        const destination = path.join(os.tmpdir(), csvFilePath);
+        await initializeIfNotAlreadyInitialized();
+        const bucket = admin.storage().bucket();
+        await bucket.file(csvFilePath).download({ destination });
+        // const csvFile: T[] = await csv;
+    }
 }
