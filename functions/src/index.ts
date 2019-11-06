40f05552f68f023a1fd00e73f3c5098c8b482abe
Zhao Shiqi <zhao.shiqi.art@gmail.com>
2019-11-05 20:59:11 -0500

    Remove unused code and imports from index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,24 +1,10 @@
 import { generateApolloServer } from "./apollo/server";
 import * as functions from "firebase-functions";
-import * as admin from "firebase-admin";
 import { Request, Response } from "express";
-import { getDataSources, Database } from "./dataSources/getDataSources";
 import { initializeIfNotAlreadyInitialized } from "./utilities/firebaseUtil";
-import * as csv from "csvtojson";
-import { Product, Facility, ProductSpec } from "./apollo/generated/types";
-import * as path from "path";
-import * as os from "os";
 import { FirestoreDbEngine } from "./dataSources/backEndDataAccess/firestoreDbEngine";
-import { ProductAndFacilityDataSource } from "./dataSources/productAndFacilitySpecDataSource";
 import { STR_CONSTANTS } from "./utilities/stringNamesReference";
 import { DataProcessor } from "./services/dataProcessor/dataProcessor";
-
-const server = generateApolloServer();
-// server.listen().then(({ url }) => {
-//     console.log(`ready at ${url}`);
-// });
-
-const RECIPE_FILE_NAME = "productRecipe.csv";
 
 export const apolloServer = functions.https.onRequest(
     async (req: Request, res: Response) => {
@@ -55,21 +41,3 @@ export const processRecipe = functions.storage
             }
         }
     });
-
-interface CsvFileFormat {
-    Product: Product;
-    ProductionTime: string;
-    BaseProduct: string;
-    ProductionFacility: Facility;
-    ProductionQuantity: string;
-    R1: Product;
-    R1C: number;
-    R2: Product;
-    R2C: number;
-    R3: Product;
-    R3C: number;
-    R4: Product;
-    R4C: number;
-    R5: Product;
-    R5C: number;
-}
